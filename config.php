e0ad952af60ba34a74fb58804c2950800eba70b6
khamdialfin <alfinpml220@gmail.com>
2025-07-16 11:42:59 +0700

    menambahkan file config,form,index dan list

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$server="localhost"
+$user="root"
+$password=""
+$nama_database="localhost"
+
+$db=mysqli_connect($server,$user,$password,$nama_database);
+if(!$db){
+    die("gagal menghubungkan database".mysqli_connect_error());
+}
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cRUD tOKO aTK</title>
+</head>
+<body>
+    <header>
+        <h3>Data Barang</h3>
+        <h1>Toko atk ANDRA</h1>
+    </header>
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-data"></a>Tambahkan Data</li>
+            <li><a href="list-data"></a>Daftar Data Barang</li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
