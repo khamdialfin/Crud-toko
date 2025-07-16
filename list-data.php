7b5db82f4d27f83575b49f7d81c044bdbd513b0f
khamdialfin <alfinpml220@gmail.com>
2025-07-16 21:43:39 +0700

    proses membuat list data

---
+++ list-data.php
@@ -0,0 +1,36 @@
+<?php include("config.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h3>Produk Yang Sudah Di inputkan</h3>
+    </header>
+    <nav>
+        <a href="form-data.php">[+] Tambah Baru</a>
+    </nav>
+    <br>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Kode Barang</th>
+                <th>Nama</th>
+                <th>Harga</th>
+                <th>Merk</th>
+                <th>Jumlah</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $sql="select " 
+            ?>
+        </tbody>
+    </table>
+    
+</body>
+</html>
